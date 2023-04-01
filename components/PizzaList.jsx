8b41bf32a2f386a20cc58aff6b58033fceb3d8ab
Aryanmehta21 <aryanmehta310@gmail.com>
2023-04-01 22:45:28 +0530

    .

---
+++ components/PizzaList.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import styles from '../styles/PizzaList.module.css'
+import PizzaCard from './PizzaCard'
+
+const PizzaList = () => {
+  return (
+    <div className={styles.container}>
+        <h1 className={styles.title}>THE BEST PIZZA IN TOWN</h1>
+        <p className={styles.desc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis rerum, accusamus natus minus aut ratione facilis cum reprehenderit odit possimus sed et ullam placeat doloremque doloribus enim, tempora dolore.
+        </p>
+        <div className={styles.wrapper}>
+            <PizzaCard/>
+            <PizzaCard/>
+            <PizzaCard/>
+            <PizzaCard/>
+            <PizzaCard/>
+            <PizzaCard/>
+            <PizzaCard/>
+            <PizzaCard/>
+        </div>
+    </div>
+  )
+}
+
+export default PizzaList
